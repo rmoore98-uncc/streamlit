3fc25ed7c005e3f19425bdeb789f637e6b577339
rmoore98-uncc <rmoore98@charlotte.edu>
2025-07-14 18:59:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@ option = st.selectbox(
     "Select a Category",
     df["Category"].unique()
 )
+filtered_df = df[df["Category"] == option]
+st.dataframe(filtered_df)
 st.write("### Input Data and Examples")
 
 
@@ -18,16 +20,16 @@ st.write("### Input Data and Examples")
 st.bar_chart(df, x="Category", y="Sales")
 
 # Now let's do the same graph where we do the aggregation first in Pandas... (this results in a chart with solid bars)
-st.dataframe(df.groupby("Category").sum())
+st.dataframe(filtered_df.groupby("Category").sum())
 # Using as_index=False here preserves the Category as a column.  If we exclude that, Category would become the datafram index and we would need to use x=None to tell bar_chart to use the index
-st.bar_chart(df.groupby("Category", as_index=False).sum(), x="Category", y="Sales", color="#04f")
+st.bar_chart(filtered_df.groupby("Category", as_index=False).sum(), x="Category", y="Sales", color="#04f")
 
 # Aggregating by time
 # Here we ensure Order_Date is in datetime format, then set is as an index to our dataframe
-df["Order_Date"] = pd.to_datetime(df["Order_Date"])
-df.set_index('Order_Date', inplace=True)
+filtered_df["Order_Date"] = pd.to_datetime(filtered_df["Order_Date"])
+filtered_df.set_index('Order_Date', inplace=True)
 # Here the Grouper is using our newly set index to group by Month ('M')
-sales_by_month = df.filter(items=['Sales']).groupby(pd.Grouper(freq='M')).sum()
+sales_by_month = filtered_df.filter(items=['Sales']).groupby(pd.Grouper(freq='M')).sum()
 
 st.dataframe(sales_by_month)
 
